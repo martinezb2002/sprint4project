d62a521d1e0638281c0bf18c094e54d4b1f3566c
Belen <martinezb2002@gmail.com>
2025-01-29 21:39:36 -0600

    Fix data type conversion for price

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import plotly.express as px
 
 df = pd.read_csv('vehicles_us.csv')
 
+df['price'] = df['price'].astype(float)
+
 # creating a Header
 st.header("Car Sales Dashboard")
 
@@ -17,6 +19,7 @@ fig = px.scatter(df, x="price", y="odometer")
 st.plotly_chart(fig)
 
 # creating a checkbox
+   
 if st.checkbox("Show Raw Data"):
     st.write(df)
 
